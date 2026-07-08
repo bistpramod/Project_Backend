69380cd1583107710108638a71be2c961c39590b
bistpramod <bistpramod113@gmail.com>
2026-07-08 16:51:14 +0545

    all ready for the multer file uploads

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-
+import categoryRoutes from "./category.routes"
 import authRoutes from "./auth.routes";
 import userRoutes from "./user.routes";
 import brandRoutes from "./brand.routes";
@@ -10,5 +10,5 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/brands", brandRoutes);
-
+router.use("/categories", categoryRoutes)
 export default router;
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import  express  from "express";
 
-import { getAllAdmins , getById , getAll} from "../controllers/user.controller";
+import { getAllAdmins , getById , getAll , deleteUser} from "../controllers/user.controller";
 
 const router = express.Router()
 //* get all users 
@@ -10,5 +10,8 @@ router.get('/admins', getAllAdmins)
 
 //* get user by id
 router.get("/:id", getById)
+//* delete user 
+router.delete("/:id", deleteUser);
+
 
 export default router
